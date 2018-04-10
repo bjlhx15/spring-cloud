9e2b6c58e8e11d905b01720c349ce9d03b9584fd
bjlhx15 <bjlhx15@163.com>
2018-04-10 22:57:07 +0800

    init

---
+++ microservice-sample-comsumer-movie/src/main/java/com/lhx/cloud/microservicesamplecomsumermovie/MicroserviceSampleComsumerMovieApplication.java
@@ -0,0 +1,12 @@
+package com.lhx.cloud.microservicesamplecomsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSampleComsumerMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSampleComsumerMovieApplication.class, args);
+	}
+}
---
+++ microservice-sample-provider-user/src/test/java/com/lhx/cloud/microservicesampleprovideruser/MicroserviceSampleProviderUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lhx.cloud.microservicesampleprovideruser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceSampleProviderUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
