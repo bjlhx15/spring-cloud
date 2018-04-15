cf86a81df67f6e1afe2ece7c44fd1118ac56e0bb
bjlhx15 <bjlhx15@163.com>
2018-04-15 22:47:40 +0800

    eureka server注册

--- microservice-provider-user/src/main/java/com/lhx/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/lhx/cloud/controller/UserController.java
@@ -1,21 +1,51 @@
 package com.lhx.cloud.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhx.cloud.entity.User;
 import com.lhx.cloud.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private EurekaClient eurekaClient;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
+	
 	@GetMapping("/sample/{id}")
 	public User findById(@PathVariable Long id) {
-		return userRepository.findById(id).get();
+		return userRepository.findOne(id);
+	}
+
+	@GetMapping("/eureka-instance")
+	public String serviceUrl() {
+	    InstanceInfo instance = eurekaClient.getNextServerFromEureka("MIRCOSERVICE-PROVIDER-USER", false);
+	    return instance.getHomePageUrl();
+	}
+	@GetMapping("/eureka-info")
+	public ServiceInstance showInfo() {
+	    ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
+	    return serviceInstance;
+	}
+	@GetMapping("/eureka-url")
+	public Object serviceUrl2() {
+	    List<ServiceInstance> list = discoveryClient.getInstances("MIRCOSERVICE-PROVIDER-USER");
+	    if (list != null && list.size() > 0 ) {
+	        return list.get(0).getUri();
+	    }
+	    return null;
 	}
 }
