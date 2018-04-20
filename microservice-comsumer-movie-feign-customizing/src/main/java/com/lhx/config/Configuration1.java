f3a3af1038664a13c9fd62e088a2e4acc86c0541
bjlhx15 <bjlhx15@163.com>
2018-04-20 21:56:21 +0800

    feign配置

---
+++ microservice-comsumer-movie-feign-customizing/src/main/java/com/lhx/config/Configuration1.java
@@ -0,0 +1,14 @@
+package com.lhx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Contract;
+
+@Configuration
+public class Configuration1 {
+	@Bean
+	public Contract feignContract() {
+		return new feign.Contract.Default();
+	}
+}
