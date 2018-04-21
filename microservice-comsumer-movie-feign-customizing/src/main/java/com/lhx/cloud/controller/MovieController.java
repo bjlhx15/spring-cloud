79fc3f0242c62bec1844914f0bfa0db019797186
bjlhx15 <bjlhx15@163.com>
2018-04-21 09:55:27 +0800

    feign配置以及日志

--- microservice-comsumer-movie-feign-customizing/src/main/java/com/lhx/cloud/controller/MovieController.java
+++ microservice-comsumer-movie-feign-customizing/src/main/java/com/lhx/cloud/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhx.cloud.entity.User;
+import com.lhx.cloud.feign.FeignClient2;
 import com.lhx.cloud.feign.UserFeignClient;
 
 @RestController
@@ -13,9 +14,16 @@ public class MovieController {
 
 	@Autowired
 	private UserFeignClient userFeignClient;
+	@Autowired
+	private FeignClient2 feignClient2;
 
 	@GetMapping("/movie/{id}")
 	public User findById(@PathVariable Long id) {
 		return userFeignClient.findById(id);
+	}
+
+	@GetMapping("/{serviceName}")
+	public String findById(@PathVariable String serviceName) {
+		return feignClient2.findServiceInfoFromEurekaByServiceName(serviceName);
 	}
 }
---
+++ microservice-comsumer-movie-feign-customizing/src/main/java/com/lhx/cloud/feign/FeignClient2.java
@@ -0,0 +1,22 @@
+package com.lhx.cloud.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lhx.cloud.entity.User;
+import com.lhx.config.Configuration1;
+import com.lhx.config.Configuration2;
+
+import feign.Param;
+import feign.RequestLine;
+
+@FeignClient(name = "xxxx", url = "http://localhost:8761/", configuration = Configuration2.class)
+public interface FeignClient2 {
+	@RequestMapping(value = "/eureka/apps/{serviceName}")
+	public String findServiceInfoFromEurekaByServiceName(@PathVariable("serviceName") String serviceName);
+}
