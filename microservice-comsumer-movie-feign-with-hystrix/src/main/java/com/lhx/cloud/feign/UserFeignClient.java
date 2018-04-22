e71ec36f4b0069f3535381db58f1c60d0203de6e
bjlhx15 <bjlhx15@163.com>
2018-04-22 21:13:11 +0800

    feign hystrix

---
+++ microservice-comsumer-movie-feign-with-hystrix/src/main/java/com/lhx/cloud/feign/UserFeignClient.java
@@ -0,0 +1,15 @@
+package com.lhx.cloud.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lhx.cloud.entity.User;
+
+@FeignClient(name = "microservice-provider-user", fallback = HystrixClientFallback.class)
+public interface UserFeignClient {
+	// @GetMapping("/sample/{id}")
+	@RequestMapping(method = RequestMethod.GET, value = "/sample/{id}")
+	public User findById(@PathVariable("id") Long id);
+}
